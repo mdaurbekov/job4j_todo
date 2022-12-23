4c0c2278c11ab780de1679c1be611dce2e2de834
mdaurbekov <dreamkov@gmail.com>
2022-12-23 10:16:19 +0300

    add UserController

---
+++ src/main/java/ru/job4j/todo/controller/UserController.java
@@ -0,0 +1,64 @@
+package ru.job4j.todo.controller;
+
+import lombok.AllArgsConstructor;
+import net.jcip.annotations.ThreadSafe;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import ru.job4j.todo.model.User;
+import ru.job4j.todo.service.UserService;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.Optional;
+
+@Controller
+@ThreadSafe
+@AllArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @PostMapping("/registration")
+    public String registration(@ModelAttribute User user) {
+        Optional<User> regUser = userService.findUserByLogin(user.getLogin());
+        if (!regUser.isEmpty()) {
+            return "redirect:/registrationPage?fail=true";
+        }
+        userService.add(user);
+        return "redirect:/loginPage";
+    }
+
+    @GetMapping("/registrationPage")
+    public String registrationPage(Model model, @RequestParam(name = "fail", required = false) Boolean fail) {
+        model.addAttribute("fail", fail != null);
+        return "registration";
+    }
+
+    @GetMapping("/loginPage")
+    public String loginPage(Model model, @RequestParam(name = "fail", required = false) Boolean fail) {
+        model.addAttribute("fail", fail != null);
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String login(@ModelAttribute User user, HttpServletRequest request) {
+        Optional<User> userDb = userService.findUserByLoginAndPassword(
+                user.getLogin(), user.getPassword()
+        );
+        if (userDb.isEmpty()) {
+            return "redirect:/loginPage?fail=true";
+        }
+        HttpSession session = request.getSession();
+        session.setAttribute("user", userDb.get());
+        return "redirect:/tasks/";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        session.invalidate();
+        return "redirect:/tasks/";
+    }
+}
